fc6a61c05f8f4cce33821242022978551f3fcb79
Abdourazak <116330614+AbdouPy@users.noreply.github.com>
2025-07-12 17:40:52 +0100

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,6 @@ const dbConfig = require("../config/db.config.js");
 const { Sequelize, DataTypes } = require("sequelize");
 
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
